9355051250658a1cbfefa8bf703c19cc64faf112
Jaey-oung <jaeywodud7@gmail.com>
2024-04-13 22:44:07 +0900

    [Programmers-81301] 숫자 문자열과 영단어

---
+++ daily-quest/src/programmers/NumericWordConverter.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class NumericWordConverter {
+	public static void main(String args[]) {
+		String s = "one4seveneight";
+
+		NumericWordConverterSolution solver = new NumericWordConverterSolution();
+		int result = solver.solution(s);
+
+		System.out.println(result);
+	}
+}
+
+class NumericWordConverterSolution {
+	private static final String[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+			"nine", "ten" };
+
+	public int solution(String s) {
+		for (int i = 0; i < numbers.length; i++) {
+			// 각 영단어와 맞는 인덱스 값으로 변환
+			s = s.replace(numbers[i], Integer.toString(i));
+		}
+		// 문자열을 정수로 변환
+		return Integer.parseInt(s);
+	}
+}
